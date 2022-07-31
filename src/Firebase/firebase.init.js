9766c059bfffd7d321348dddb75b24adc75a7ffe
shadik <shadikmahamudsohan@gmail.com>
2022-07-31 21:58:05 +0600

    adding new firebase config

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAyS94xuOJ1JbZC7AoJXuowF9CvIrSk8OM",
-    authDomain: "fack-facebook.firebaseapp.com",
-    projectId: "fack-facebook",
-    storageBucket: "fack-facebook.appspot.com",
-    messagingSenderId: "1074177497506",
-    appId: "1:1074177497506:web:07a7525dfb01832477fafb"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
